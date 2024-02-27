500ca5f037ad13bf617deb96e40f54ee39d4249f
yeyaozu <yaozuye@zju.edu.cn>
2024-02-28 01:43:12 +0800

    默认支持英文符号自动配对,可以在表格编辑时生效

--- src/main.ts
+++ src/main.ts
@@ -86,7 +86,8 @@ export default class EasyTypingPlugin extends Plugin {
 		this.IntrinsicDeleteRules = ruleStringList2RuleList(DeleteRulesStrList);
 
 		let autoPairRulesPatchStrList: Array<[string, string]> = [["【】|】", "【】|"], ["（）|）", "（）|"],
-		["<>|>", "<>|"], ["《》|》", "《》|"], ["「」|」", "「」|"], ["『』|』", "『』|"], ["()|)", "()|"], ['[]|]', '[]|']
+		["<>|>", "<>|"], ["《》|》", "《》|"], ["「」|」", "「」|"], ["『』|』", "『』|"], ["()|)", "()|"], ['[]|]', '[]|']，
+		["{}|}", "{}|"]
 		];
 		this.IntrinsicAutoPairRulesPatch = ruleStringList2RuleList(autoPairRulesPatchStrList);
 
