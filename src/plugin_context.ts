5b8029d8d665e03a57769ebdea1a6838e3d528ed
yeyaozu <yaozuye@zju.edu.cn>
2026-02-15 16:00:53 +0800

    refactor: 添加 PluginContext 接口定义

---
+++ src/plugin_context.ts
@@ -0,0 +1,21 @@
+import { App } from 'obsidian';
+import { EasyTypingSettings, PairString } from './settings';
+import { RuleEngine } from './rule_engine';
+import { LineFormater } from './core';
+
+export interface PluginContext {
+	settings: EasyTypingSettings;
+	ruleEngine: RuleEngine;
+	Formater: LineFormater;
+	app: App;
+	halfToFullSymbolMap: Map<string, string>;
+	TaboutPairStrs: PairString[];
+	compose_begin_pos: number;
+	compose_end_pos: number;
+	compose_need_handle: boolean;
+	CurActiveMarkdown: string;
+	onFormatArticle: boolean;
+	getDefaultIndentChar(): string;
+	isCurrentFileExclude(): boolean;
+	saveSettings(): Promise<void>;
+}
